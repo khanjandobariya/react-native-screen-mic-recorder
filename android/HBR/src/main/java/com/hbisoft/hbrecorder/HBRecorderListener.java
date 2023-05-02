e1b67abe77c2ddfc0ce35314651cc593a4a916d6
Solanki Yogesh <yogesh.s@arccus.in>
2023-05-02 18:30:31 +0530

    fix:removed unused

--- android/HBR/src/main/java/com/hbisoft/hbrecorder/HBRecorderListener.java
+++
@@ -1,7 +0,0 @@
-package com.hbisoft.hbrecorder;
-
-public interface HBRecorderListener {
-    void HBRecorderOnStart();
-    void HBRecorderOnComplete();
-    void HBRecorderOnError(int errorCode, String reason);
-}
